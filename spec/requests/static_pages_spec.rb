35dce88d50207192f6d5eb24a8eef752da8f5b3d
swechter <swechter@gmail.com>
2013-08-25 14:55:43 -0400

    chapter 3 exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,18 @@ describe "About page" do
         end
   end
 
+describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the title 'Contact" do
+          visit '/static_pages/contact'
+          page.should have_selector('title',
+            :text => "Ruby on Rails Tutorial Sample App | Contact")
+        end
+  end
+
+
 end
